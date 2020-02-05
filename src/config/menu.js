c36ea9fa10d53b0a545424cb562249ccc22694c5
syin <e_suyin@Outlook.com>
2020-02-05 10:51:07 +0800

    调整菜单路由

--- src/config/menu.js
+++ src/config/menu.js
@@ -12,89 +12,141 @@ const menus = [
 		controller: 'index',
 		action: 'profile',
 		is_hidden: 0,
-		children: [
-			{
-				name: '列表',
-				icon: '',
-				controller: 'index',
-				action: 'list',
-				is_hidden: 0,
-				children: []
-			},
-			{
-				name: '表单',
-				icon: '',
-				controller: 'index',
-				action: 'form',
-				is_hidden: 1,
-				children: []
-			},
-		]
 	},
 	{
 		name: '页面模板',
 		icon: '',
 		controller: 'demo',
-		action: 'index',
+		action: 'indexman',
 		is_hidden: 0,
 		children: [
 			{
 				name: '列表',
 				icon: '',
 				controller: 'demo',
-				action: 'list',
+				action: 'listman',
 				is_hidden: 0,
-				children: []
+				children: [
+					{
+						name: '一般列表',
+						icon: '',
+						controller: 'demo',
+						action: 'list-basic',
+						is_hidden: 0,
+						children: []
+					},
+					{
+						name: '筛选列表',
+						icon: '',
+						controller: 'demo',
+						action: 'list-filter',
+						is_hidden: 0,
+						children: []
+					},
+					{
+						name: '卡片列表',
+						icon: '',
+						controller: 'demo',
+						action: 'list-card',
+						is_hidden: 0,
+						children: []
+					},
+				]
+			},
+			{
+				name: '详情',
+				icon: '',
+				controller: 'demo',
+				action: 'profileman',
+				is_hidden: 1,
+				children: [
+					{
+						name: '基础详情页',
+						icon: '',
+						controller: 'demo',
+						action: 'profile-basic',
+						is_hidden: 0,
+						children: []
+					},
+					{
+						name: '高级详情页',
+						icon: '',
+						controller: 'demo',
+						action: 'profile-basic',
+						is_hidden: 0,
+						children: []
+					}
+				]
 			},
 			{
 				name: '表单',
 				icon: '',
 				controller: 'demo',
-				action: 'form',
+				action: 'formman',
 				is_hidden: 1,
-				children: []
-			},
-			{
-				name: '筛选',
-				icon: '',
-				controller: 'demo',
-				action: 'filter',
-				is_hidden: 0,
 				children: [
 					{
-						name: '3级-1',
+						name: '基础表单',
 						icon: '',
 						controller: 'demo',
-						action: 'lv31',
+						action: 'form-basic',
 						is_hidden: 0,
-
-						children: [
-							{
-								name: '4级-1',
-								icon: '',
-								controller: 'demo',
-								action: 'lv41',
-								is_hidden: 0,
-								
-							},
-							{
-								name: '4级-2',
-								icon: '',
-								controller: 'demo',
-								action: 'lv42',
-								is_hidden: 0,
-							},
-							{
-								name: '4级-3',
-								icon: '',
-								controller: 'demo',
-								action: 'lv43',
-								is_hidden: 0,
-							}
-						]
-					}
+						children: []
+					},
+					{
+						name: '高级表单',
+						icon: '',
+						controller: 'demo',
+						action: 'form-advanced',
+						is_hidden: 0,
+						children: []
+					},
 				]
 			},
+			{
+				name: '日历',
+				icon: '',
+				controller: 'demo',
+				action: 'calendar',
+				is_hidden: 0,
+			},
+			{
+				name: '锁屏',
+				icon: '',
+				controller: 'demo',
+				action: 'lock',
+				is_hidden: 0,
+			},
+		]
+	},
+	{
+		name: '异常页',
+		icon: '',
+		controller: 'abnormal',
+		action: 'indeman',
+		is_hidden: 0,
+		children: [
+			{
+				name: '403',
+				icon: '',
+				controller: 'abnormal',
+				action: '403',
+				is_hidden: 0
+			},
+			{
+				name: '404',
+				icon: '',
+				controller: 'abnormal',
+				action: '404',
+				is_hidden: 0
+			},
+			{
+				name: '500',
+				icon: '',
+				controller: 'abnormal',
+				action: '500',
+				is_hidden: 0,
+			}
 		]
 	},
 ];
