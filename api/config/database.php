f2a0f2ed0c6f3eed8542596ce493dfec007f0c96
syin <e_suyin@Outlook.com>
2020-02-21 17:16:28 +0800

    备份数据库, 解除相应thinkphp文件夹限制方便下载

--- api/config/database.php
+++ api/config/database.php
@@ -15,7 +15,7 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'schedule',
+    'database'        => 'dashboard',
     // 用户名
     'username'        => 'sy',
     // 密码
---
+++ dashboard.sql
@@ -0,0 +1,42 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 本机3607
+Source Server Version : 50726
+Source Host           : localhost:3307
+Source Database       : dashboard
+
+Target Server Type    : MYSQL
+Target Server Version : 50726
+File Encoding         : 65001
+
+Date: 2020-02-21 17:15:47
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for sy_admin
+-- ----------------------------
+DROP TABLE IF EXISTS `sy_admin`;
+CREATE TABLE `sy_admin` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `login_name` varchar(128) NOT NULL DEFAULT '' COMMENT '登录账号',
+  `name` varchar(128) NOT NULL DEFAULT '' COMMENT '名称',
+  `password` varchar(64) NOT NULL DEFAULT '' COMMENT '登录密码',
+  `avatar` varchar(128) NOT NULL DEFAULT '' COMMENT '头像',
+  `logged_count` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '登录次数',
+  `is_admin` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `is_disabled` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否禁用; 0: 否, 1: 是',
+  `is_deleted` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否删除; 0: 否, 1: 是',
+  `add_time` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '添加时间',
+  `update_time` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '修改时间',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='管理员';
+
+-- ----------------------------
+-- Records of sy_admin
+-- ----------------------------
+INSERT INTO `sy_admin` VALUES ('1', 'test', 'syin', '$2y$10$lYM5gt1xCyCzwiGXZlfzGOgkc16AMwQ8BB9.qxBU8OU20LGgk0x56', '/static/common/img/avatar/20.png', '0', '0', '0', '0', '1558075424', '1561367143');
+INSERT INTO `sy_admin` VALUES ('2', 'root', '测试用户', '$2y$10$8YLyrp0Kb3HlwE5oK./P/OguFDOv23AXrI58jIJJtAoS6Hnk5avRW', '/static/img/avatar/29.png', '0', '0', '0', '0', '1558323222', '1558495729');
+INSERT INTO `sy_admin` VALUES ('3', '管理员', '111', '$2y$10$iSKNZWTGtdyZ0VELqhX6F.y/4zKm5g4F3ao1DW1dodJ1Ym/DuRV2a', '/static/img/avatar/29.png', '0', '0', '0', '0', '1559290020', '1559290020');
